3f429a19510e130d7d67865a66a10c9d485ef5a4
Carto Wong <cartowong@gmail.com>
2016-09-12 13:07:19 -0700

    build a trigram model

--- week3/buildModel.R
+++ week3/buildModel.R
@@ -1,4 +1,6 @@
 source('util.R')
+bigramModelRDataFilePath <- 'bigramModel.rData'
+trigramModelRDataFilePath <- 'trigramModel.rData'
 
 blogsTokens <- tokenize('sampleData/en_US.blogs.sample.txt')$tokens
 newsTokens <- tokenize('sampleData/en_US.news.sample.txt')$tokens
@@ -6,7 +8,8 @@ twitterTokens <- tokenize('sampleData/en_US.twitter.sample.txt')$tokens
 combinedTokens <- c(blogsTokens, newsTokens, twitterTokens)
 
 bigramModel <- buildNGramModel(combinedTokens, n = 2)
-save(bigramModel, file = 'bigramModel.rData')
+save(bigramModel, file = bigramModelRDataFilePath)
 
 trigramModel <- buildNGramModel(combinedTokens, n = 3)
-save(trigramModel, file = 'trigramModel.rData')
+save(trigramModel, file = trigramModelRDataFilePath)
+  
