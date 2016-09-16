2ea48568b3d6eaa42d1ce1b65f1c92372d4148fc
Carto Wong <cartowong@gmail.com>
2016-09-16 13:25:53 -0700

    re-compute bigram and trigram frequencies after the refactoring of utility functions

---
+++ model/buildModel.R
@@ -0,0 +1,33 @@
+# Build n-gram models.
+
+source('nGramUtil.R')
+
+# You may change this parameter to any number between 0 and 1. The code below will continue to work.
+p <- 0.05
+
+tokenDir <- paste('data', as.integer(100 * p), sep = '/')
+tokenFilePath <- paste(tokenDir, 'tokens.rData', sep = '/')
+outputDir <- paste('model', as.integer(100 * p), sep = '/')
+
+if (!dir.exists(outputDir)) {
+  dir.create(outputDir)
+}
+
+getOutputPath <- function(fileName) {
+  paste(outputDir, fileName, sep = '/')
+}
+
+# Load the tokens.
+load(tokenFilePath)
+
+# Compute bigram frequencies.
+message('Computing bigram frequencies...')
+bigramFreq <- nGramFreq(tokens, n = 2)
+
+# Compute trigram frequencies.
+message('Computing trigram frequencies...')
+trigramFreq <- nGramFreq(tokens, n = 3)
+
+# Save the bigram and trigram frequencies.
+message(paste0('Saving bigram and trigram frequencies into ', outputDir, '/freq.rData'))
+save(bigramFreq, trigramFreq, file = paste0(outputDir, '/freq.rData...'))
